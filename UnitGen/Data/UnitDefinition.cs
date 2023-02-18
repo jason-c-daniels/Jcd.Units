dd30566b0553e0a3bb58a9ea64129d921b84b503
Jason C. Daniels <jason.c.daniels@gmail.com>
2023-02-17 20:39:06 -0600

    updated UnitDefinition to properly prefix things from the root Jcd.Units.UnitsOfMeasure namespace.

--- UnitGen/Data/UnitDefinition.cs
+++ UnitGen/Data/UnitDefinition.cs
@@ -13,12 +13,14 @@ public record UnitDefinition
     public bool IsBaseUnit => Unit.IsBaseUnit && Prefix.IsBasePrefix;
     public string UnitVarName => $"{UnitName}".MakeSymbolName();
 
-    public string BaseUnitNamespacePrefix => 
-        Unit.HasBaseUnitSubnamespace
-            ? string.IsNullOrWhiteSpace(Unit.BaseUnitSubnamespace)
-                ? $"{UnitType.EnumerationName}."
-                : $"{Unit.BaseUnitSubnamespace}.{UnitType.EnumerationName}."
-            : "";
+    public string BaseUnitNamespacePrefix { get {
+            return Unit.HasBaseUnitSubnamespace && Prefix.IsBasePrefix
+                ? string.IsNullOrWhiteSpace(Unit.BaseUnitSubnamespace)
+                    ? $"UnitsOfMeasure.{UnitType.EnumerationName}."
+                    : $"{Unit.BaseUnitSubnamespace}.{UnitType.EnumerationName}."
+                : "";
+        }
+    }
     public string Coefficient => Prefix.IsBasePrefix ? Unit.Coefficient : Prefix.Coefficient;
     public string Offset => Unit.Offset;
 
@@ -29,5 +31,8 @@ public record UnitDefinition
         : $"{Prefix.BasePrefix}{Unit.UnitName}"
     ;
     public string Symbol => IsBaseUnit ? Unit.UnitSymbol : $"{Prefix.Symbol}{Unit.UnitSymbol}";
-    public string Subnamespace => System.HasSubnamespace ? $".{System.Subnamespace}" : "";
+    public string Subnamespace => System.HasSubnamespace 
+        ? 
+        $".{System.Subnamespace}" 
+        : "";
 }
