db05cd03081897c57356cfb8f39e717c317df83d
Jason C. Daniels <jason.c.daniels@gmail.com>
2023-03-07 21:20:56 -0600

    Implemented the uber-unit test to verify all generated (or hand coded) enumerations actually have units of measure defined.

---
+++ Jcd.Units.Tests/GeneratedUnitsTests.cs
@@ -0,0 +1,52 @@
+﻿#region
+
+using System.Collections;
+using System.Reflection;
+
+#endregion
+
+namespace Jcd.Units.Tests;
+
+public class GeneratedUnitsTests
+{
+   [Theory]
+   [MemberData(nameof(EnumerationsTypesAndUnitOfMeasureInstances))]
+   public void All_Enumerations_GetAll_Returns_NonEmpty_Collections(Type enumerationType, IEnumerable unitsOfMeasure)
+   {
+      Assert.NotNull(enumerationType);
+      Assert.NotNull(unitsOfMeasure);
+      Assert.NotEmpty(unitsOfMeasure);
+   }
+
+   /// <summary>
+   /// Use reflection to 
+   /// </summary>
+   public static IEnumerable<object[]> EnumerationsTypesAndUnitOfMeasureInstances
+   {
+      get
+      {
+         // enumerates all types implementing Enumeration<TEnumeration,TUnit>
+         var enumerationTypes =
+                  from type in Assembly.GetAssembly(typeof(Enumeration<,>))!.GetTypes()
+                  let baseType = type.BaseType
+                  where !type.IsAbstract
+                     && !type.IsInterface
+                     && baseType is not null
+                     && baseType.IsGenericType
+                     && baseType.GetGenericTypeDefinition() == typeof(Enumeration<,>)
+                  select type;
+
+         foreach (var enumerationType in enumerationTypes)
+         {
+            // bind the GetAll method.
+            var baseType = enumerationType.BaseType;
+            var method   = baseType?.GetMethod("GetAll", BindingFlags.Public | BindingFlags.Static);
+
+            // call GetAll
+            var results = method?.Invoke(null, null) as IEnumerable;
+
+            yield return new object[] { enumerationType, results! };
+         }
+      }
+   }
+}
